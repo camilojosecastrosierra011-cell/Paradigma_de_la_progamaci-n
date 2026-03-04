7f718afcefa06f2a6b95fa0095c3bef91a85743d
¨camilojosecastrosierra011¨ <camilojosecastrosierra011@gmail.com>
2026-03-04 15:36:40 -0500

    Funciones Puras

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function Suma(x) {
+    let accion = x + 5;
+    return accion;
+};
+
+console.log(Suma(7));
+
+function Divicion (x) {
+    let accion = x/5;
+    return accion;
+};
+
+console.log(Divicion(20));
