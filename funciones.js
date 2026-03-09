6296581e27e42c7ecf7c346c3f96dac86774604a
¨camilojosecastrosierra011¨ <camilojosecastrosierra011@gmail.com>
2026-03-09 13:03:47 -0500

    FUNCIONES.JS

--- funciones.js
+++ funciones.js
@@ -5,9 +5,39 @@ function Suma(x) {
 
 console.log(Suma(7));
 
-function Divicion (x) {
+function Dividicion(x) {
     let accion = x/5;
     return accion;
 };
 
-console.log(Divicion(20));
+console.log(Dividicion(25));
+
+
+let resultado = 0;
+function SumarPrecios(a, b) {
+    return resultado = resultado + a * b;
+};
+
+console.log(SumarPrecios(4, 6));
+console.log(SumarPrecios(2, 3));
+console.log(SumarPrecios(5, 8));
+
+
+let palabra = "";
+function UniondePalabras(a) {
+    return palabra = palabra + a;
+};
+
+console.log(UniondePalabras("Hola Mundo"));
+console.log(UniondePalabras("Bendiones pa la familia"));
+
+
+function Multiplicar(a) {
+    return a * a;
+};
+
+function SumaDeLaMultiplicaion(num1, num2) {
+    return Multiplicar(num1) + Multiplicar(num2);
+};
+
+console.log(SumaDeLaMultiplicaion(3, 4));
