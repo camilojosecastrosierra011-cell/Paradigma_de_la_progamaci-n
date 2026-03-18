331d33bb905718d54aeb74f4e98bcb259d3e30b1
¨camilojosecastrosierra011¨ <camilojosecastrosierra011@gmail.com>
2026-03-18 16:11:59 -0500

    TALLER DE DB.SON

--- Backend/Mi_primer_servidor.js
+++ Backend/Mi_primer_servidor.js
@@ -26,19 +26,3 @@ app.get('/datos', (req, res) =>{
 app.listen(3000, () => {
     console.log("Servidor ejecutándose en el puerto 3000");
 });
-
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hola Mundo PCJIC');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
-
-
-
-
